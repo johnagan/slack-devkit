1c55e95e3e1d9a1c1c685e88a623ffe1f415e6a3
John Agan <john@slack-corp.com>
2018-08-12 14:13:31 -0700

    payload and example updates

--- examples/features.js
+++ examples/features.js
@@ -1,59 +1,79 @@
-const { PORT, SIGNING_SECRET, CLIENT_ID, CLIENT_SECRET, SCOPE } = process.env
+const Slack = require('slack-devkit')
 
-const Slack = require('../')
-
+// Configure express with the Slack App settings
 const { server, client } = new Slack({
-  scope: SCOPE,
-  client_id: CLIENT_ID,
-  client_secret: CLIENT_SECRET,
-  signing_secret: SIGNING_SECRET
+  scope: 'commands,files:write,links:read,links:write',
+  client_id: "1212313.1231231231231",
+  client_secret: "12312312323123123",
+  signing_secret: "sdfsadfsadfasdfas"
 })
 
-
-// Slack OAuth Route
+// All GET routes redirect to the “Add to Slack” OAuth flow
 server.get('/', (req, res) => {
+  // the req.slack object contains information about the request
+  // and the workspace's authentication information
   const { data, app_url } = req.slack
-  const { ok, installer_user } = data
 
-  // an error happened during oauth
-  if (!ok) return res.json(data)
-
-  // Send a welcome message to the installer
+  // Make an authenticated request to the Slack API
   req.slack.api('chat.postMessage', {
-    channel: installer_user.app_home,
+    channel: data.installer_user.app_home,
     text: 'Thanks for installing me :bow:'
   })
 
-  // redirect to Slack
+  // open the Slack client to the App Home
   res.redirect(app_url)
 })
 
-
-// Slash Command route to upload a file
+// Slash Command and Events API routes automatically load the
+// workspace info and related datastore
 server.post('/slash-command', (req, res) => {
-  const fileUrl = 'https://d3vv6lp55qjaqc.cloudfront.net/items/0g1j143z0u341v0N4536/slack_image.png'
+  // check if a sub-command was sent
+  const isUpload = req.slack.isSubCommand('upload')
 
-  client.get(fileUrl).then(r => {
-    const file = Buffer.from(r.data, 'utf8')
-    req.slack.upload({ filename: 'slack_image.png', file })
-  })
+  // test the message text with regex
+  const containsUrl = req.slack.match(/https?:\/\//i)
+
+  // use the included client to call other APIs or
+  // to load information from other sites
+  if (isUpload && containsUrl) {
+    // get the arguments from a sub-command
+    const fileUrl = req.slack.subcommand('upload')
+    client.get(fileUrl).then(r => {
+      const file = Buffer.from(r.data, 'utf8')
+
+      // one-line of code to respond to slash commands with a file
+      req.slack.upload({ filename: 'logo.png', file })
+    })
+  }
 
   res.send()
 })
 
 
-// Events API route to unfurl
-server.post('/unfurl', (req, res) => {
-  req.slack.unfurl({
-    text: "text",
-    title: "title",
-    title_link: "https://link.com",
-    image_url: "https://image.com"
-  })
+// All POST routes expect Slack callback events
+// and verify against the verification token
+server.post('/', (req, res) => {
+  // check the event type with is()  
+  const isUnfurl = req.slack.is('link_shared')
+  const isMessage = req.slack.is('message.app_home')
+
+  // respond with an unfurl easily
+  if (isUnfurl) {
+    req.slack.unfurl({
+      text: "A successful unfurl!",
+      image_url: "https://image.com"
+    })
+  }
+
+  // reply to messages everywhere
+  if (isMessage) {
+    req.slack.reply({
+      text: "Hey! I got your message :sunglasses:"
+    })
+  }
 
   res.send()
 })
 
-
-// Start webserver
-server.start(PORT)
+// Start the webserver on port 3000
+server.start(3000)
