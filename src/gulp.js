4481d1f438a03863f50966d58ea09324f9f56494
John Agan <john@slack-corp.com>
2018-08-15 09:48:41 -0700

    added router docs

--- src/gulp.js
+++ src/gulp.js
@@ -1,9 +1,9 @@
-const gulp = require('gulp')
-const pug = require('gulp-pug')
-const concat = require('gulp-concat')
-const data = require('./data.json')
-const DOCS_PATH = '../docs'
-
+const
+  gulp = require('gulp'),
+  pug = require('gulp-pug'),
+  concat = require('gulp-concat'),
+  data = require('./data.json'),
+  DOCS_PATH = '../docs'
 
 gulp.task('css', function() {
   return gulp.src('docs/css/*.css')
